b7ecf0318a95269d074676e519e219a27c32efa2
FabianAronsson <fabian.aronsson@elev.ga.ntig.se>
2020-09-08 13:05:22 +0200

    Commit

---
+++ HelloWorld/Metoder2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Metoder2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(Subtract(Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())));
+        }
+
+        private static int Subtract (int firstInt, int secondInt)
+        {
+            return (firstInt - secondInt);
+        }
+
+        private static List<String> Add()
+        {
+            return (firstInt - secondInt);
+        }
+    }
+}
