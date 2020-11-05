4cd4298d8a9f52a0b3a078e47c7ceb975f0b4545
FabianAronsson <fabian.aronsson@elev.ga.ntig.se>
2020-11-05 11:35:30 +0100

    Skapade model och den grafiska applikationen.

--- Numpad/Numpad/MainWindow.xaml.cs
+++ Numpad/Numpad/MainWindow.xaml.cs
@@ -25,10 +25,54 @@ namespace Numpad
             InitializeComponent();
         }
 
-        private void  Click1(object sender, RoutedEventArgs e)
+        private void ButtonClick(object sender, RoutedEventArgs e)
         {
-            
+            Model model = new Model();
+            if(sender is Button button)
+            {
+                switch (button.Content)
+                {
+                    case "0":
+                    case "1":
+                    case "2":
+                    case "3":
+                    case "4":
+                    case "5":
+                    case "6":
+                    case "7":
+                    case "8":
+                    case "9":
+                        TextField.Text += button.Content;
+                        break;
+                    case "+":
+                        model.Numbers.Add(Int32.Parse(TextField.Text));
+                        model.Operator.Add('+');
+                        break;
+                    case "-":
+                        model.Numbers.Add(Int32.Parse(TextField.Text));
+                        model.Operator.Add('-');
+                        break;
+                    case "EXE":
+
+                        if(model.Numbers.Count > 1)
+                        {
+                            var result = 0;
+                            for (int i = 0; i < model.Numbers.Count; i++)
+                            {
+                                
+                                var operators = model.Operator[i];
+                                result += model.Numbers[i] operators model.Numbers[i + 1];
+
+                            }
+
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
+
 
         
     }
---
+++ Numpad/Numpad/Model.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Numpad
+{
+    class Model
+    {
+        private List<int> numbers = new List<int>();
+
+        public List<int> Numbers
+        {
+            get { return numbers; }
+            set { numbers = value; }
+        }
+
+        private List<char> calcChoice;
+
+        public List<char> Operator
+        {
+            get { return calcChoice; }
+            set { calcChoice = value; }
+        }
+
+
+    }
+}
